68aa2b8d0b0b3b4dfb4c3dc6d84d7939451e9bd1
Adam Murdoch <adam@gradle.com>
2018-12-27 09:20:55 +1100

    Fixes for changes to error message formats.

--- subprojects/platform-base/src/integTest/groovy/org/gradle/language/base/BinariesLifecycleTaskIntegrationTest.groovy
+++ subprojects/platform-base/src/integTest/groovy/org/gradle/language/base/BinariesLifecycleTaskIntegrationTest.groovy
@@ -17,6 +17,7 @@
 package org.gradle.language.base
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.internal.logging.text.DiagnosticsVisitor
 import org.hamcrest.Matchers
 
 class BinariesLifecycleTaskIntegrationTest extends AbstractIntegrationSpec {
@@ -41,7 +42,7 @@ class BinariesLifecycleTaskIntegrationTest extends AbstractIntegrationSpec {
                         }
 
                         @Override
-                        public void explain(org.gradle.util.TreeVisitor<? super String> visitor) {
+                        public void explain(${DiagnosticsVisitor.name} visitor) {
                             visitor.node("Binary \${name} has 'notBuildable' in the name")
                         }
                     };
@@ -68,11 +69,11 @@ class BinariesLifecycleTaskIntegrationTest extends AbstractIntegrationSpec {
 
         then:
         failureDescriptionContains("Execution failed for task ':assemble'.")
-        failure.assertThatCause(Matchers.<String>allOf(
-            Matchers.startsWith("No buildable binaries found:"),
-            Matchers.containsString("SampleBinary 'lib:notBuildableBinary1': Binary notBuildableBinary1 has 'notBuildable' in the name"),
-            Matchers.containsString("SampleBinary 'lib:notBuildableBinary2': Binary notBuildableBinary2 has 'notBuildable' in the name")
-        ))
+        failure.assertHasCause("""No buildable binaries found:
+  - SampleBinary 'lib:notBuildableBinary1':
+      - Binary notBuildableBinary1 has 'notBuildable' in the name
+  - SampleBinary 'lib:notBuildableBinary2':
+      - Binary notBuildableBinary2 has 'notBuildable' in the name""")
     }
 
     def "builds those component binaries that are buildable and skips those that are not" () {
